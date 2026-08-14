a34f22ce0b7303663d16bb473203b1371ee8e2c6
sorokoladov7-gif <sorokoladov7@gmail.com>
2026-08-14 06:56:26 +0400

    config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,3 @@
 const SUPABASE_URL      = 'https://ulxfsozdryqrnlxzlblt.supabase.co';   // ← вставь
 const SUPABASE_ANON_KEY = 'sb_publishable_9hmWZwV5WnfQHDK1ir36Pg_JIdHdwPq';                   // ← вставь
 const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-const DELIVERY_FEE = 150;
-const DEFAULT_IMG = 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?auto=format&fit=crop&q=80&w=400';
