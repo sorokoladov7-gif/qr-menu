d279094b7712760d162110939038715bda4d2306
sorokoladov7-gif <sorokoladov7@gmail.com>
2026-08-22 23:33:27 +0400

    chore: remove obsolete table management stub

--- js/manager-tables.js
+++
@@ -1,5 +0,0 @@
-// Заглушка: этот модуль использовал ПРЯМЫЕ запросы к venue_tables (insert/update/delete)
-// в обход RPC, что небезопасно. Управление столами идёт через:
-//   - вкладку "🪑 Зал / Столы" в manager.html (RPC manager_table_board)
-//   - tables.html (отдельная страница столов)
-console.info('[manager-tables.js] Заглушка. Столы управляются через вкладку в manager.html или tables.html');
