7b4763b78c99e80c8c1770a889e3561515241b5c
Алексей <sorokoladov7@gmail.com>
2026-09-01 16:36:53 +0400

    fix(manager): register recipes module in app composition

--- js/manager/manager-app.js
+++ js/manager/manager-app.js
@@ -11,7 +11,8 @@
     window.__QR_MANAGER_STAFF_MIXIN__,
     window.__QR_MANAGER_BILLING_MIXIN__,
     window.__QR_MANAGER_ANALYTICS_MIXIN__,
-    window.__QR_MANAGER_SETTINGS_MIXIN__
+    window.__QR_MANAGER_SETTINGS_MIXIN__,
+    window.__QR_MANAGER_RECIPES_MIXIN__
   ];
 
   var appData = function(){
